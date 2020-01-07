c99b65ea2bc79a12ed5b6d23a4f4b4ddca214c03
Chase Sillevis <chase@sillevis.net>
2020-01-07 11:29:03 +0100

    version detection not working inside module

--- modules/beta-private-cluster/versions.tf
+++ modules/beta-private-cluster/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 0.12"
 
-  required_providers {
-    google-beta = ">= 2.18, <4.0.0"
-  }
+  //required_providers {
+  //  google-beta = ">= 2.18, <4.0.0"
+  //}
 }
--- modules/private-cluster/versions.tf
+++ modules/private-cluster/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 0.12"
 
-  required_providers {
-    google = ">= 2.18, <4.0.0"
-  }
+  //required_providers {
+  //  google = ">= 2.18, <4.0.0"
+  //}
 }
